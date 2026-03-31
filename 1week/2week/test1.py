6994800a2825445dc556ea1f37ef9481204cec40
0E <92708940+Yum02@users.noreply.github.com>
2026-03-31 11:09:28 +0000

    20260331 commit

--- 1week/2week/test1.py
+++ 1week/2week/test1.py
@@ -12,3 +12,20 @@ elif score < 60 and score >= 0:
     print("F")
 else :
     print("Error : 0~100 사이의 숫자를 입력해주세요.")
+
+score = int(input("점수 입력 : "))
+
+if score <0 or score > 100:
+    print("Error : 0~100 사이의 숫자를 입력해주세요.")
+elif score >= 90:
+    print("A")
+elif score >= 80 and score < 90:
+    print("B")
+elif score >= 70 and score < 80:
+    print("C")
+elif score >= 60 and score < 70:
+    print("D")
+elif score < 60 and score >= 0:
+    print("F")
+else :
+    print("Error : 0~100 사이의 숫자를 입력해주세요.")
